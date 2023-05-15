159e4211df8b5e69a007d1c71454eb54deb43b35
rithgan <rithikgandhi1989@gmail.com>
2023-05-16 00:11:30 +0530

    token

--- src/contract/token.js
+++ src/contract/token.js
@@ -25,3 +25,10 @@ export const checkApprove = async (provider,tokenAddress,tokenAbi,userAddress,sp
     let result = await cont.balanceOf(userAddress)
     return  parseFloat(ethers.utils.formatUnits(result, 18)).toFixed(3)
 }
+
+export const transfer = async(provider,tokenAddress,tokenAbi,recipientAddress,amount)=>{
+  let cont = await contract(provider,tokenAddress,tokenAbi)
+  let res =  await cont.approve(recipientAddress,amount)
+  let conf = res.wait()
+  return conf
+}
